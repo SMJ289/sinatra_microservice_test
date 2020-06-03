89c94f42e6d94aff9898f6fb0dd4a7d7e2bfc4c9
Steven Meyers <sjjmeyers@gmail.com>
2020-06-02 22:47:17 -0600

    Hide api key

--- lib/youtube_service.rb
+++ lib/youtube_service.rb
@@ -12,7 +12,7 @@ class YouTubeService
 
   def self.conn
     Faraday.new(url: 'https://www.googleapis.com/youtube/v3') do |f|
-      f.params['key'] = 'AIzaSyAYvomeKFpJV7vuCo6EYQdwbzehrqrqOAw'
+      f.params['key'] = ''
     end
   end
 end
